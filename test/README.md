023ca1555ab42f77488e4119462b5b2d10f723a7
Jeremy Banka <jeremybanka@gmail.com>
2021-04-02 09:02:57 -0700

    ✅ can mr roboger count to 10?

--- test/README.md
+++ test/README.md
@@ -3,3 +3,7 @@
 Test: "Return an array with a 0 if the number 0 is inputted"
 Code: beepBoop(0);
 Expected Output: \[0]
+
+Test: "Mr Roboger counts to ten"
+Code: beepBoop(10);
+Expected Output: \[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
--- test/core.test.js
+++ test/core.test.js
@@ -5,3 +5,9 @@ test(`mr roboger hands you back your zero`, () => {
   const expected = [0]
   expect(robogerCount(input)).toEqual(expected)
 })
+
+test(`mr roboger counts to ten`, () => {
+  const input = 10
+  const expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  expect(robogerCount(input)).toEqual(expected)
+})
